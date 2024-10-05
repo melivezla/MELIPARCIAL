dc304b20442f9c8bc17c84f5a52f11a90c03c0e6
melivezla <131054190+melivezla@users.noreply.github.com>
2024-10-04 21:54:15 -0500

    Guardar cambios locales antes de merge

--- obj/Debug/net8.0/MELIPARCIAL.AssemblyInfo.cs
+++ obj/Debug/net8.0/MELIPARCIAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MELIPARCIAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4bba0e93198e07e1a4d2d814c86a325a22b8e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("MELIPARCIAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MELIPARCIAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
